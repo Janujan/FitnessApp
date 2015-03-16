87fcb4d1aa0dfcd61a81b91a43cf78dbcc8ec5fd
Janujan Selvaratnam <janujan.selva@gmail.com>
2015-03-16 00:00:50 -0400

    Edited button layout and added new activity for workout

--- FitnessApp/app/src/main/java/com/example/janujanselva/fitnessapp/startWorkout.java
+++ FitnessApp/app/src/main/java/com/example/janujanselva/fitnessapp/startWorkout.java
@@ -1,28 +1,26 @@
 package com.example.janujanselva.fitnessapp;
 
-import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
+import android.content.Intent;
 
-
-public class MainActivity extends ActionBarActivity implements View.OnClickListener{
+public class startWorkout extends ActionBarActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.activity_start_workout);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_start_workout, menu);
         return true;
     }
 
@@ -41,24 +39,13 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
         return super.onOptionsItemSelected(item);
     }
 
-    public void onClick(View v){}
+    public void onClick(View v){
 
-    public void startWrk(View v){
-
-        Toast.makeText(this,"You clicked the start workout button", Toast.LENGTH_SHORT).show();
-        startActivity(new Intent(MainActivity.this, startWorkout.class));
     }
 
-    public void hist(View v){
-        Toast.makeText(this,"You clicked the history button", Toast.LENGTH_LONG).show();
-    }
+    public void returnMain(View v){
 
-    public void changeWrk(View v){
-        Toast.makeText(this,"You clicked the change workout button", Toast.LENGTH_LONG).show();
-    }
-
-    public void settings(View v){
-        Toast.makeText(this,"You clicked the settings button", Toast.LENGTH_LONG).show();
-
+        Toast.makeText(this, "Returning to main activity", Toast.LENGTH_SHORT).show();
+        startActivity(new Intent(startWorkout.this, MainActivity.class));
     }
 }
